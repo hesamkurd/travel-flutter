e14e83670d1ad02fe04ea8329cf29c3d73bea657
mamhesam <mamhesam1@gmail.com>
2023-05-24 00:42:16 +0330

    work with Stack widget and Positioned

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:travel_flutter/model/travel_model.dart';
+import 'package:travel_flutter/util/app_layout.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,116 +13,104 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MainPageState extends State<MainPage> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              width: double.infinity,
+              height: size.height / 1.8,
+              color: Colors.red,
+              child: Stack(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: size.height / 2.1,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(
+                          AppLayout.getHeight(55),
+                        ),
+                        bottomRight: Radius.circular(
+                          AppLayout.getHeight(55),
+                        ),
+                      ),
+                      image: DecorationImage(
+                        image: AssetImage(
+                          travelList[_selectedIndex].img,
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  // Head Icons
+                  Positioned(
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    child: Padding(
+                      padding: EdgeInsets.fromLTRB(
+                        AppLayout.getWidth(16),
+                        AppLayout.getHeight(20),
+                        AppLayout.getWidth(16),
+                        0,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: AppLayout.getWidth(50),
+                            height: AppLayout.getHeight(50),
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0x68FFFFFF),
+                            ),
+                            child: const Icon(CupertinoIcons.arrow_left),
+                          ),
+                          Container(
+                            width: AppLayout.getWidth(50),
+                            height: AppLayout.getHeight(50),
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0x68FFFFFF),
+                            ),
+                            child: const Icon(CupertinoIcons.heart),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Colors.blue,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/model/travel_model.dart
@@ -0,0 +1,60 @@
+class TravelModel {
+  String name;
+  String location;
+  String price;
+  String temp;
+  String distance;
+  String dsecription;
+  String img;
+  String rating;
+
+  TravelModel({
+    required this.name,
+    required this.location,
+    required this.price,
+    required this.temp,
+    required this.distance,
+    required this.dsecription,
+    required this.img,
+    required this.rating,
+  });
+}
+
+List<TravelModel> travelList = [
+  TravelModel(
+      name: "Awyar",
+      location: "Sine - Kurdistan",
+      price: "7000",
+      temp: "27",
+      distance: "215",
+      dsecription: "dsecription",
+      img: "assets/images/1.webp",
+      rating: "4.9"),
+  TravelModel(
+      name: "Arbaba",
+      location: "bane - Kurdistan",
+      price: "6500",
+      temp: "26",
+      distance: "125",
+      dsecription: "dsecription",
+      img: "assets/images/2.jpg",
+      rating: "4.5"),
+  TravelModel(
+      name: "shalmash",
+      location: "Serdasht - Kurdistan",
+      price: "6400",
+      temp: "25",
+      distance: "143",
+      dsecription: "dsecription",
+      img: "assets/images/3.jpg",
+      rating: "4.6"),
+  TravelModel(
+      name: "rejaw",
+      location: "serpel - Kurdistan",
+      price: "7200",
+      temp: "27",
+      distance: "234",
+      dsecription: "dsecription",
+      img: "assets/images/4.jpg",
+      rating: "4.8"),
+];
